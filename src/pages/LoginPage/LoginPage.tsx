48e88d863117fb0013d6388d3370b1b47bf3413a
Anton Efremenkov <anton.efremenkov@itentika.ru>
2023-05-01 18:46:07 +0300

    login page fixed

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,5 +1,10 @@
 export const LoginPage = () => {
   return (
-    <h1>LoginPage works!!</h1>
+    <>
+      <h1>Login page works!!</h1>
+      <form>
+        <button type="submit">Let me in!!</button>
+      </form>
+    </>
   );
 };
